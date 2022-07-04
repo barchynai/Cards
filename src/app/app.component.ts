6faa726c91a3f4ea5621e917d1efa7e636794c90
Barchynai Kochkonbai kyzy <barchynai_kochkonbai_kyzy@epam.com>
2022-07-04 17:41:24 +0600

    passing data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Cards';
+posts = [
+  {
+    title: 'Neat Tree',
+    imageUrl: 'assets/tree.jpeg',
+    username: '@nature',
+    content: 'I saw this tree today'
+  },
+  {
+    title: 'Snowy Mountain',
+    imageUrl: 'assets/mountain.jpeg',
+    username: '@mountainlover',
+    content: 'There is a picture of snowy mountain'
+  },
+  {
+    title: 'Mountain Biking',
+    imageUrl: 'assets/biking.jpeg',
+    username: '@biking222',
+    content: 'I did some biking today'
+  },
+]
+
 }
